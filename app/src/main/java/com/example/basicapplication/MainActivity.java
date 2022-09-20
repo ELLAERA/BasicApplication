362e0bcd9b8f1b000f388cd78fdd5cc462efdbaf
aera_lee <qwer240777@gmail.com>
2022-09-20 13:54:26 +0900

    ListView

--- app/src/main/java/com/example/basicapplication/MainActivity.java
+++ app/src/main/java/com/example/basicapplication/MainActivity.java
@@ -5,12 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,15 +23,27 @@ public class MainActivity extends AppCompatActivity {
     private EditText et_test;
     private String str;
     private ImageView test_img;
+    private ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // ListView
+        list = (ListView) findViewById(R.id.list);
+        List<String> data = new ArrayList<>();
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data);
+        list.setAdapter(adapter);
+
+        data.add("apple");
+        data.add("banana");
+        data.add("carrot");
+        adapter.notifyDataSetChanged();
+
+        // Button
         et_test = findViewById(R.id.et_test);
         btn_move = findViewById(R.id.btn_move);
-
         btn_move.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -37,11 +54,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //ImageView
         test_img = (ImageView) findViewById(R.id.test_img);
         test_img.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Hello, there!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "@+string/hello", Toast.LENGTH_SHORT).show();
             }
         });
     }
